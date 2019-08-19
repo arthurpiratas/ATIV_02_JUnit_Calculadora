de44b3d436f3c9d5ae3f8a593c9f0d66a23ac6ea
Arthur Soares Cavalcante Silva <silva.arthursoares@gmail.com>
2019-08-18 21:29:29 -0300

    Add files via upload

---
+++ CalculadoraTeste/src/ICalculadora.java
@@ -0,0 +1,7 @@
+
+public interface ICalculadora {
+	
+	public int somar(String num1, String num2); 
+	public double dividir (String num1, String num2); 
+	public double rauzQuadrada(String num1); 
+}
