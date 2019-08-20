cb83cf9a5c83bf88602092b5ae51dc30d90a18ce
Arthur Soares Cavalcante Silva <silva.arthursoares@gmail.com>
2019-08-19 21:50:40 -0300

    Delete ICalculadora.java

--- CalculadoraTeste/src/ICalculadora.java
+++
@@ -1,7 +0,0 @@
-
-public interface ICalculadora {
-	
-	public int somar(String num1, String num2); 
-	public double dividir (String num1, String num2); 
-	public double rauzQuadrada(String num1); 
-}
